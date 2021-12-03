652e65b1bc0078b5e421026c5ae487c4def26651
shaunvan119 <shaunvan@live.com.au>
2021-12-03 11:18:34 +1100

    css header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
     return (
@@ -29,13 +30,18 @@ function Header() {
 
             </div>
             <div className="header_option">
-               <span className="header_optionLineOne">Your p</span>
-               <span className="header_optionLineTwo">Sign In</span>
+               <span className="header_optionLineOne">Your</span>
+               <span className="header_optionLineTwo">Prime</span>
 
+            </div>
+
+            <div className="header_optionBasket">
+                <ShoppingBasketIcon/>
+                <span className="header_optionLineTwo header_basketCount">0</span>
             </div>
          </div>
         </div>
-    )
+    );
 }
 
 export default Header
