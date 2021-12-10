05311d13bd221d28a147d538202d5235b54f4189
shaunvan119 <shaunvan@live.com.au>
2021-12-10 16:01:17 +1100

    state provider and reducer

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
     return (
         <div className="checkout">
-          <h1>like button</h1>  
+          <div className="checkout_left">
+             <img 
+             className="checkout_ad" 
+             src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
+             alt=""/>
+
+             <div className="checkout_title">
+                 <h2>Your shopping basket</h2>
+             </div>
+          </div>
+
+          <div className="checkout_right">
+          <Subtotal/>
+          
+          </div>
         </div>
     );
 }
--- src/Header.js
+++ src/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className='header'>
+        <Link to="/">
             <img 
             className="header_logo" 
             src="https://m.media-amazon.com/images/G/35/gc/designs/livepreview/amazon_logo_noto_email_v2016_au-main._CB444479176_.png"
             alt="" />
-
+        </Link>
+        
             <div className="header_search">
              <input 
              className="header_searchInput" 
@@ -34,12 +37,15 @@ function Header() {
                <span className="header_optionLineTwo">Prime</span>
 
             </div>
-
+            <Link to="/checkout">
             <div className="header_optionBasket">
                 <ShoppingBasketIcon/>
                 <span className="header_optionLineTwo header_basketCount">0</span>
-            </div>
+             </div>
+            </Link>
          </div>
+
+         
         </div>
     );
 }
